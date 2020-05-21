586f54f55851ecfe11385755eac93976e57071ef
PROX-E <lichlord18@gmail.com>
2020-05-21 14:05:39 -0600

    update book controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -8,5 +8,11 @@ module.exports = {
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 }
