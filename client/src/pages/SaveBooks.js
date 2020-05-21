7b5aafb123c34d6ac15ba4d7df1c39bb3976f365
PROX-E <lichlord18@gmail.com>
2020-05-21 14:23:00 -0600

    update saved books.js

---
+++ client/src/pages/SaveBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Jumbotron from "../components/Jumbotron";
+import { Container} from "../components/Grid";
+import SavedResult from "../components/SavedResult"
+
+class SaveBook extends Component {
+    state = {
+        savedBooks: []
+    };
+
+}
+
+
+
+export default SaveBook 
