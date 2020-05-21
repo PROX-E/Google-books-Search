5bc4653b2d9b5411ad0ad91d93766b80cea5eb5e
PROX-E <lichlord18@gmail.com>
2020-05-21 13:46:46 -0600

    update grid

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -4,3 +4,9 @@ import React from "react";
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
+
+
+// Row component to let us use a bootstrap row without class names
+export function Row({ fluid, children }) {
+    return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+  }
