4fa66bbd6c4999edf3626cbf904d97437725b01c
PROX-E <lichlord18@gmail.com>
2020-05-21 14:11:14 -0600

    update server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require ("mongoose");
+const routes= require ("./routes")
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Define middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static assets 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// API routes 
+app.use(routes)
+
+//Connet to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
