6b5b64ba46a91e179e1402d867fb7ba03eac9318
PROX-E <lichlord18@gmail.com>
2020-05-21 15:33:23 -0600

    update build

---
+++ client/build/precache-manifest.33429959b782ecea2480d49e045a116a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c86ea8a4f543f8647ad29f011f75052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1abdf0a75821de0526c",
+    "url": "/static/css/main.92d0a782.chunk.css"
+  },
+  {
+    "revision": "659922d12eb70a12f47a",
+    "url": "/static/js/2.191a578b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.191a578b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1abdf0a75821de0526c",
+    "url": "/static/js/main.944a6511.chunk.js"
+  },
+  {
+    "revision": "82af21258e8866d037ae",
+    "url": "/static/js/runtime-main.6acab3be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
